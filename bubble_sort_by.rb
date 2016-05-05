1f8a85f0c95e238372c5435a590df053b6882d54
Ali SARIALTUN <alisarialtun@outlook.com>
2016-05-05 18:32:42 +0000

    works with and without block

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,27 +2,23 @@ def bubble_sort_by(arr)
   swapped = true
   while swapped
     swapped=false
-    for i in 0..arr.length-2
+    for i in 0..arr.length-2 do
+       if block_given?
         if yield(arr[i], arr[i+1])>0
             arr[i], arr[i+1] = arr[i+1], arr[i]
             swapped=true
+        end #if yield
+      else
+        if arr[i] > arr[i+1]
+          arr[i], arr[i+1] = arr[i+1], arr[i] 
+          swapped=true
         end
-    end
-  end
+      end #if block_given?
+    end #for
+  end #while
   puts arr
   return arr
 end
 
-
-bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}
 bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length}
 bubble_sort_by([8,1,4,3,7])
-
-=begin
-def test
-   yield 5
-   puts "You are in the method test"
-   yield 100
-end
-test {|i| puts "You are in the block #{i}"}
-=end
